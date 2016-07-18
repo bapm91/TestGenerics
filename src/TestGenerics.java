a608ac89649475c08356851c1febc50c177acbe1
Slava Kulynych <Kulynych.v.a@gmail.com>
2016-07-18 18:33:29 +0300

    Переделал для компиляции

--- src/TestGenerics.java
+++ src/TestGenerics.java
@@ -7,15 +7,15 @@ public class TestGenerics {
     } // method main
 
     public void go() {
-        ArrayList<Dog> dogs1 = new ArrayList<Animal>(); // 1
-        ArrayList<Animal> animals1 = new ArrayList<Dog>(); // 2
+        List<Dog> dogs1 = new ArrayList<Dog>();//ArrayList<Dog> dogs1 = new ArrayList<Animal>(); // 1
+        List<? extends Animal> animals1 = new ArrayList<Dog>();//ArrayList<Animal> animals1 = new ArrayList<Dog>(); // 2
         List<Animal> list = new ArrayList<Animal>(); // 3
         ArrayList<Dog> dogs = new ArrayList<Dog>(); // 4
-        ArrayList<Animal> animals = dogs; // 5
+        List<? extends Animal> animals = dogs;//ArrayList<Animal> animals = dogs; // 5
         List<Dog> dogList = dogs; // 6
         ArrayList<Object> objects = new ArrayList<Object>(); // 7
         List<Object> objList = objects; // 8
-        ArrayList<Object> objs = new ArrayList<Dog>(); // 9
+        List<? extends Object> objs = new ArrayList<Dog>(); //ArrayList<Object> objs = new ArrayList<Dog>(); // 9
     } // method go
 
     public void takeAnimals(ArrayList<Animal> animals) {
